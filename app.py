e41164ebf7ba86fbddba5959a6dcfd0fad23755a
marchelinoboyoh <s21710442@student.unklab.ac.id>
2024-03-20 16:19:53 +0800

    testing-1

---
+++ app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    with open("index.html", "r") as f:
+        html_content = f.read()
+    return HTMLResponse(content=html_content)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
